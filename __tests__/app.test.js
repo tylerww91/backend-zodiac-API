5ba49a7694eb82ec08799cb7a4ecdd5cfad89802
Tyler Watson <tylerww91@gmail.com>
2022-11-01 14:17:57 -0700

    changes /signs to /zodiac to match rubric and fixed the test and app.js to match that change. Builds route to /zodiac which passes the test that was previously built

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,11 +5,11 @@ const app = require('../lib/app');
 const { signs } = require('../lib/zodiac-data.js');
 
 describe('signs routes', () => {
-  it('/signs should return a list of signs', async () => {
-    const res = await request(app).get('/signs');
+  it('/zodiac should return a list of zodiac signs with their names and ids', async () => {
+    const res = await request(app).get('/zodiac');
     const expected = signs.map((sign) => {
       return { id: sign.id, name: sign.name };
     });
-    expected(res.body).toEqual(expected);
+    expect(res.body).toEqual(expected);
   });
 });
