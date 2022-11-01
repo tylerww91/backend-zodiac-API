3607350517ee3ffcf821d6968366541ceee799c4
Tyler Watson <tylerww91@gmail.com>
2022-11-01 13:01:57 -0700

    adds zodiac-data.js containing the signs array, adds first test for /signs to return the list of signs

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,15 @@
 const request = require('supertest');
+
 const app = require('../lib/app');
 
-describe('example test - you should probably update me', () => {
-  it('home route should return hello world', async () => {
-    const resp = await request(app).get('/');
-    expect(resp.text).toEqual('hello world!');
+const { signs } = require('../lib/zodiac-data.js');
+
+describe('signs routes', () => {
+  it('/signs should return a list of signs', async () => {
+    const res = await request(app).get('/signs');
+    const expected = signs.map((sign) => {
+      return { id: sign.id, name: sign.name };
+    });
+    expected(res.body).toEqual(expected);
   });
 });
